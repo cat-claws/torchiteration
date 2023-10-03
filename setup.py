e5485dc471cc85bb51b52369451a1aca01ffb42a
cat-claws <47313357+cat-claws@users.noreply.github.com>
2023-10-03 19:14:28 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+readme = open('README.md').read()
+
+VERSION = '0.0.1'
+
+requirements = [
+    'numpy',
+    'pandas',
+	'torch'
+]
+
+setup(
+    # Metadata
+    name='torchiteration',
+    version=VERSION,
+    author='cat-claws',
+    author_email='47313357+cat-claws@users.noreply.github.com',
+    url='https://github.com/cat-claws/torchiteration',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+
+    # Package info
+    packages=find_packages(exclude=('*test*',)),
+
+    #
+    zip_safe=True,
+    install_requires=requirements,
+
+    # Classifiers
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
